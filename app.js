a179a065428d70c61011473c2c547a7913ba61d5
Sabrola <70954761+Sabrola@users.noreply.github.com>
2023-02-28 13:59:50 +0000

    [28/02] Método Retrieve do CRUD

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,13 +12,27 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Conexão ao BD ------------------------------------------
+const db = require('./models')
+
+try {
+  db.sequelize.authenticate()
+  console.log('SEQUELIZE: connection has been established successfully.')
+}
+catch(error) {
+  console.error('* SEQUELIZE: unable to connect to the database: ', error)
+  process.exit(1)     // Encerra o servidor com erro
+}
+// ---------------------------------------------------------
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/********************* ROTAS ***************************/
+const users = require('./routes/users')
+app.use('/users', users)
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,54 @@
+// Importar o model correspondente ao controller
+const { User } = require('../models')
+
+const controller = {}   // Objeto vazio
+
+/*
+  Métodos CRUD do controller
+  create: cria um novo registro
+  retrieve: lista (recupera) todos os registros
+  retrieveOne: lista (recupera) apenas um registro
+  update: atualiza um registro
+  delete: exclui um registro
+*/
+
+controller.create = async (req, res) => {
+  try {
+    await User.create(req.body)
+    // HTTP 201: Created
+    res.status(201).end()
+  }
+  catch(error) {
+    console.error(error)
+  }
+}
+
+controller.retrieve = async (req, res) => {
+  try {
+    const data = await User.findAll()
+    
+    //HTTP 200: OK (Implicito)
+    res.send(data)
+  }
+  catch {
+    console.error(error)
+  }
+}
+
+controller.retrieveOne = async (req, res) => {
+  try {
+    const data = await User.findByPk(req.params.id)
+    
+    //HTTP 200: OK (Implicito)
+    if(data) res.send(data)
+
+    //HTTP 404 (Not Found)
+    else res.status(404).end()
+    
+  }
+  catch(error) {
+    console.error(error)
+  }
+}
+
+module.exports = controller
