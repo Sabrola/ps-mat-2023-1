0f112276520b4ef81d5b32dba10d03e053efb5b3
Sabrola <70954761+Sabrola@users.noreply.github.com>
2023-04-18 13:56:57 +0000

    [18/04] Correção prova/recriação front-end

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Home() {
+    return (
+        <>
+            <h1>Página Inicial</h1>
+            <p>Futura página inicial</p>
+        </>
+    )
+}
---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <>
+            <h1>Página de Login</h1>
+            <p>Futura pagina de login</p>
+        </>
+    )
+}
